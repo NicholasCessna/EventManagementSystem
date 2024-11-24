cf3271c1c32ecd18cbf45722df7feb69534ec3b8
Nicholas Cessna <157065013+NicholasCessna@users.noreply.github.com>
2024-11-24 15:37:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
-# Code tying everything together akin to project main
+# app.py
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from models import db  # Import the initialized db from models
+from flask_routes import register_routes
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///event_app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'yoursecretkey'
+
+db.init_app(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    from models import User
+    return User.query.get(int(user_id))
+
+register_routes(app)
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
